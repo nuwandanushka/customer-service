2bf36dc1b354d1cc251bf79d1d172f9ae3402407
Nuwan <nuwan.wickramasooriya@theoptimum.tech>
2024-01-23 03:00:52 +0800

    implementation of customer-service microservice

--- src/test/java/com/demo/customerservice/BaseCustomerServiceApplicationTest.java
+++ src/test/java/com/demo/customerservice/BaseCustomerServiceApplicationTest.java
@@ -2,9 +2,11 @@ package com.demo.customerservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-class CustomerserviceApplicationTests {
+@ActiveProfiles("test")
+public class BaseCustomerServiceApplicationTest {
 
 	@Test
 	void contextLoads() {
